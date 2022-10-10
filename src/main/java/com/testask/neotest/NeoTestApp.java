44fa6becd6761548aa5f737abba5d8e47d323e63
Dmitry <sivolapik2018@gmail.com>
2022-10-10 20:05:14 +0300

    added pom.xml and main class

---
+++ src/main/java/com/testask/neotest/NeoTestApp.java
@@ -0,0 +1,11 @@
+package com.testask.neotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoTestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NeoTestApp.class, args);
+    }
+}
